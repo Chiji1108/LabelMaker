23f21addef61b8efb13dec3860a2ac2b476569bf
chiji1108 <shingo.pad@gmail.com>
2025-01-28 19:37:55 +0900

    Initial Commit

---
+++ LabelMaker/LabelMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelMakerApp.swift
+//  LabelMaker
+//
+//  Created by 千々岩真吾 on 2025/01/28.
+//
+
+import SwiftUI
+
+@main
+struct LabelMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
